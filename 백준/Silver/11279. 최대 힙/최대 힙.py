20137d1c52e2f39d92b70b7bb3e2ac0e913f026c
Linsio <103421762+hyeondle@users.noreply.github.com>
2024-08-28 05:09:24 +0900

    [Silver II] Title: 최대 힙, Time: 180 ms, Memory: 47076 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,11 @@
+import sys
+import heapq
+data = list(map(int,sys.stdin.read().splitlines()))
+heap = []
+for i in range(data[0]) :
+    if data[i+1] == 0 :
+        if len(heap) == 0 :
+            print(0)
+        else :
+            print(heapq.heappop(heap)[1])
+    heapq.heappush(heap, (-data[i+1],data[i+1]))
